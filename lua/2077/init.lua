48d293d473aeb8ba7e4bc577db41f094d3f7965a
mayonster <mayonster@mayonster.com>
2026-05-06 01:31:44 -0400

    Change file names to stylistically call my own theme

--- lua/2077/init.lua
+++ lua/2077/init.lua
@@ -1,5 +1,5 @@
 local M = {}
-local theme = require('bhs_2077.theme')
+local theme = require('2077.theme')
 
 M.setup = function()
   vim.cmd('hi clear')
@@ -10,7 +10,7 @@ M.setup = function()
   end
 
   vim.o.termguicolors = true
-  vim.g.colors_name = 'bhs-2077'
+  vim.g.colors_name = '2077'
 
   theme.set_highlights()
 end
